e138f2bce89d96704bc6e01ade796a50f360e687
Asiliia <51196040+Asiliia@users.noreply.github.com>
2020-02-26 01:40:19 +0300

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15; 
+const RATE_CONSTANT = 0.693 / 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+  module.exports = function dateSample(sampleActivity) {
+    if (typeof sampleActivity !== 'string' 
+                              || sampleActivity instanceof String
+                              || /[a-zA-Z]/g.test(sampleActivity) 
+                              || sampleActivity <= 0
+                              || sampleActivity > MODERN_ACTIVITY) {
+        return false;
+    }
+
+    return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / RATE_CONSTANT);    
 };
