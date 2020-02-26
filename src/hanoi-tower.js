8798d6626360858312403afc2813e6e6f604140f
Asiliia <51196040+Asiliia@users.noreply.github.com>
2020-02-26 23:58:39 +0300

    feat: add hanoi tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,7 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    const MIN_TURNS = Math.pow(2, disksNumber) - 1;
+    return {
+        turns: MIN_TURNS,
+        seconds: MIN_TURNS / (turnsSpeed / 3600)
+    }
 }
--- src/what-season.js
+++ src/what-season.js
@@ -10,28 +10,28 @@ if(Object.keys(date).length > 0) {
 
 let season;
 
-    switch(date.getMonth()) {
-      case 11:
-      case 0:
-      case 1:
-          season = 'winter';
-      break;
-      case 2:
-      case 3:
-      case 4:
-          season = 'spring';
-      break;
-      case 5:
-      case 6:
-      case 7:
-          season = 'summer';
-      break;
-      case 8:
-      case 9:
-      case 10:
-          season = 'fall';
-      break;
-    }
+switch(date.getMonth()) {
+  case 11:
+  case 0:
+  case 1:
+   season = 'winter';
+  break;
+  case 2:
+  case 3:
+  case 4:
+    season = 'spring';
+  break;
+  case 5:
+  case 6:
+  case 7:
+    season = 'summer';
+  break;
+  case 8:
+  case 9:
+  case 10:
+    season = 'fall';
+  break;
+  }
 
   return season;
 };
