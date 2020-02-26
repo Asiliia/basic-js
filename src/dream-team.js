029461a8b6b3ba1c429a8878debc1daa8314b73f
Asiliia <51196040+Asiliia@users.noreply.github.com>
2020-02-26 17:43:40 +0300

    feat: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,20 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+		return false;
+	}
+ 
+  return members
+    .filter(m => (typeof m === 'string'))
+    .map(m => 
+      m.trim()
+        .replace(/\n{2,}/g, '\n\n')
+        .replace(/ +/g, ' ')
+        [0].toUpperCase()
+      )
+  	.sort(function(a, b){
+      if((a.charCodeAt(0) - 96) < (b.charCodeAt(0) - 96)) { return -1; }
+      if((a.charCodeAt(0) - 96) > (b.charCodeAt(0) - 96)) { return 1; }
+    })
+    .join('');
+
 };
