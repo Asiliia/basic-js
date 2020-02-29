ad0d4a3e6fbac86551c3b70832d47d4ca9b74d15
Asiliia <51196040+Asiliia@users.noreply.github.com>
2020-03-01 02:49:51 +0300

    feat: add task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,30 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
+  param:[],
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+     this.param.push(`( ${value} )`);
+     return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if(typeof(position) !== 'number' 
+      || !Number.isInteger(position)
+      || this.param[position]=== undefined
+      || position < 1 
+      || position > this.param.length
+      ){
+        this.param=[];
+        throw new Error('Error');
+    }
+    this.param.splice(position-1,1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.param.reverse();
+    return this;
   },
-  finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+  finishChain() { 
+    let array = [...this.param];
+    this.param = [];
+    return array.join('~~');
   }
 };
 
